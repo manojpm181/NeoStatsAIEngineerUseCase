2474cf268cbedff3b01eafc9aa237b11258911ed
Manoj P M <159712848+manojpm181@users.noreply.github.com>
2026-03-13 13:36:19 +0530

    Update embeddings.py

--- models/embeddings.py
+++ models/embeddings.py
@@ -1,12 +1,9 @@
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from config.config import EMBEDDING_MODEL
 
+
 def get_embedding_model():
     embeddings = HuggingFaceEmbeddings(
         model_name=EMBEDDING_MODEL
     )
     return embeddings
-
-    except Exception as e:
-
-        raise RuntimeError(f"Embedding model error: {str(e)}")
