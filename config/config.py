a732825842781a49f151cb3c507d4fe12227ddbe
Manoj P M <159712848+manojpm181@users.noreply.github.com>
2026-03-13 13:42:30 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -9,5 +9,5 @@ GROQ_API_KEY = st.secrets.get("GROQ_API_KEY") or os.getenv("GROQ_API_KEY")
 SERPAPI_API_KEY = st.secrets.get("SERPAPI_API_KEY") or os.getenv("SERPAPI_API_KEY")
 
 # MODELS
-GROQ_MODEL = "llama3-8b-8192"
+GROQ_MODEL = "llama-3.1-8b-instant"
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
