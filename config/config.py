caba5b55c70f703293d5ec7e68ad1962f93d8edc
Manoj P M <159712848+manojpm181@users.noreply.github.com>
2026-03-13 13:32:48 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -4,7 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# API KEYS
 GROQ_API_KEY = st.secrets.get("GROQ_API_KEY") or os.getenv("GROQ_API_KEY")
 SERPAPI_API_KEY = st.secrets.get("SERPAPI_API_KEY") or os.getenv("SERPAPI_API_KEY")
 
+# MODELS
 GROQ_MODEL = "llama3-8b-8192"
+EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
