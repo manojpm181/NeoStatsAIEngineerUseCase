bacc17aa39fc31f51eb9fa16ebacb1cb45680f2c
Manoj P M <159712848+manojpm181@users.noreply.github.com>
2026-03-13 13:28:53 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,14 +1,10 @@
+import streamlit as st
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
+GROQ_API_KEY = st.secrets.get("GROQ_API_KEY") or os.getenv("GROQ_API_KEY")
+SERPAPI_API_KEY = st.secrets.get("SERPAPI_API_KEY") or os.getenv("SERPAPI_API_KEY")
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-
-GROQ_MODEL = "llama-3.3-70b-versatile"
-
-EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
-
-SERP_API_KEY = os.getenv("SERP_API_KEY")
-
+GROQ_MODEL = "llama3-8b-8192"
